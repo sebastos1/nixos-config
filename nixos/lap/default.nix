ac620dc37ad508bcbbc58fd4c6d054266b5a7108
sebastos1 <sjallabong@gmail.com>
2026-03-04 01:03:23 +0100

    nvm

--- nixos/lap/default.nix
+++ nixos/lap/default.nix
@@ -36,9 +36,6 @@
     };
   };
 
-  # Enable sound.
-  services.pulseaudio.enable = true;
-
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
