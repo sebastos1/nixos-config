6330bcacf2640e2531f15e5dd2d1bbcbb56ce8a4
sebastos1 <sjallabong@gmail.com>
2026-03-04 01:01:42 +0100

    lighter lap

--- nixos/lap/default.nix
+++ nixos/lap/default.nix
@@ -17,6 +17,13 @@
     HandleHibernateKey = "ignore";
   };
 
+  systemd.targets = {
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    hybrid-sleep.enable = false;
+  };
+
   security.polkit.enable = true;
   services.greetd = {
     enable = true;
@@ -29,44 +36,8 @@
     };
   };
 
-  systemd.targets = {
-    sleep.enable = false;
-    suspend.enable = false;
-    hibernate.enable = false;
-    hybrid-sleep.enable = false;
-  };
-
-  virtualisation.docker = {
-    enable = true;
-    daemon.settings = {
-      dns = ["192.168.1.1" "1.1.1.1" "8.8.8.8"];
-      ipv6 = false;
-    };
-  };
-
-  services.cloudflared = {
-    enable = true;
-    tunnels = {
-      "ec27b1d3-eae0-464a-a76f-a1bd9a16426a" = {
-        credentialsFile = "/etc/cloudflared/tunnel.json";
-        default = "http_status:404";
-        ingress = {
-          "shlb.ng" = "http://localhost:3000";
-          "sjallabong.eu" = "http://localhost:3000";
-          "pool.sjallabong.eu" = "http://localhost:8080";
-          "auth.sjallabong.eu" = "http://localhost:3001";
-        };
-      };
-    };
-  };
-
   # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+  services.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
@@ -74,19 +45,6 @@
   users.users.seb = {
     isNormalUser = true;
     extraGroups = ["wheel" "networkmanager" "docker"];
-  };
-
-  # services.openssh = {
-  #   enable = true;
-  #   settings = {
-  #     PasswordAuthentication = false; # todo?
-  #     PermitRootLogin = "no";
-  #    };
-  # };
-
-  services = {
-    avahi.enable = false;
-    fail2ban.enable = true;
   };
 
   boot.kernel.sysctl = {
