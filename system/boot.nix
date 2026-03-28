3f1eb7d925a2b5b1368ceae64082a30477609b19
sebastos1 <sjallabong@gmail.com>
2026-03-28 19:11:42 +0100

    ?

--- system/boot.nix
+++ system/boot.nix
@@ -7,20 +7,25 @@
     plymouth = {
       enable = true;
       theme = "ello";
-      themePackages = [inputs.ello-plymouth.packages.${pkgs.system}.default];
+      themePackages = [
+        (pkgs.adi1090x-plymouth-themes.override {
+          selected_themes = ["rings"];
+        })
+        inputs.ello-plymouth.packages.${pkgs.system}.default
+      ];
     };
 
     # silent boot
     consoleLogLevel = 3;
     initrd.verbose = false;
     kernelParams = [
-      "nvidia-drm.fbdev=1"
+      # prevent the kernel from blanking plymouth out of the fb
+      "fbcon=nodefer"
+      # disable boot logo if any
+      "logo.nologo"
       "quiet"
       "udev.log_level=3"
       "systemd.show_status=auto"
-
-      # show only errors or worse in udev
-      "rd.udev.log_level=3"
     ];
     # hide OS choice. Can still be opened by pressing any key
     loader.timeout = 0;
