861216354fb83119fcc74c52576ff47924730491
sebastos1 <sjallabong@gmail.com>
2026-04-02 17:31:46 +0200

    bruh

--- hosts/homeserver/dns.nix
+++ hosts/homeserver/dns.nix
@@ -1,4 +1,3 @@
-{ ... }:
 {
   perSystem =
     { pkgs, ... }:
@@ -7,17 +6,17 @@
         terraformWrapper.package = pkgs.opentofu;
         modules = [
           (
-            { lib, ... }:
             let
               tunnel = "29cf8536-1108-4b81-8cbb-ff6d84cdf120";
-              target = "${tunnel}.cfargotunnel.com";
-              zone = "71fc4efd9ff85d6e65f7bac4f1f8f91d";
-              mkRecord = hostname: {
+              shlb = "e30b7a9147aac2d6283478a2c4d96919";
+              sjallabong = "71fc4efd9ff85d6e65f7bac4f1f8f91d";
+              mkRecord = zone: name: {
                 zone_id = zone;
-                name = hostname;
-                content = target;
+                name = name;
+                content = "${tunnel}.cfargotunnel.com";
                 type = "CNAME";
                 proxied = true;
+                ttl = 1;
               };
             in
             {
@@ -31,12 +30,12 @@
               provider.cloudflare.api_token = "\${var.cloudflare_api_token}";
 
               resource.cloudflare_dns_record = {
-                ssh_shlb_ng = mkRecord "ssh.shlb.ng";
-                dash_shlb_ng = mkRecord "dash.shlb.ng";
-                pool_sjallabong = mkRecord "pool.sjallabong.com";
-                account_sjallabong = mkRecord "account.sjallabong.com";
-                matrix_sjallabong = mkRecord "matrix.sjallabong.com";
-                root_sjallabong = mkRecord "sjallabong.com";
+                ssh_shlb_ng = mkRecord shlb "ssh";
+                dash_shlb_ng = mkRecord shlb "dash";
+                pool_sjallabong = mkRecord sjallabong "pool";
+                account_sjallabong = mkRecord sjallabong "account";
+                matrix_sjallabong = mkRecord sjallabong "matrix";
+                root_sjallabong = mkRecord sjallabong "@";
               };
             }
           )
