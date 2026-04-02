b8b9ab061287f36f26ccb7a65505cd47296d1f33
sebastos1 <sjallabong@gmail.com>
2026-04-02 20:15:52 +0200

    sdaf

--- hosts/homeserver/default.nix
+++ hosts/homeserver/default.nix
@@ -104,8 +104,7 @@ in
 
   systemd.tmpfiles.rules = [
     "d /var/lib/microvms/matrix-vm/journal 0755 root root -"
-    "L+ /var/log/journal/0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a - - - -
-    /var/lib/microvms/matrix-vm/journal/0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a"
+    "L+ /var/log/journal/0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a - - - - /var/lib/microvms/matrix-vm/journal/0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a"
   ];
 
   age.secrets.cf-tunnel-json.file = ../../secrets/cf-tunnel-json.age;
